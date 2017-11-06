59b5677fcd24a193d088f1f7a24026f55c9df78a
maryli891 <marytta095@gmail.com>
2017-11-06 18:32:17 -0300

    Se crea nueva clase Meses del año y se realiza el encabezado

---
+++ src/pruebajava2/MesesdelAño.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+
+/**
+ * Mariane Muñoz Gonzalez
+ * Fecha 06 de Noviembre  2017
+ * @author LC22
+ * Clase Mesesdelaño
+ * Responsabilidad Clase principal
+ *
+ */
+public class MesesdelAño 
+{
+    
+}
